768cb21d2cdc47e77bcc8a777e8d12a58715d0c9
Christopher Ryan Combs Jr <ryancombsjr@gmail.com>
2018-02-02 12:20:26 -0700

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
+  /// IF ARGC == 2
   struct config config_values = read_config(argv[1]);
   struct metadata *md = read_metadata(config_values.filepath);
 
